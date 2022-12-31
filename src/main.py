612e60f62dd04eb7be3908b8594a4872b21c786c
Aliasgar Abid Sabunwala <aliasgarabidsabunwala@gmail.com>
2022-12-31 19:38:07 +0530

    main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# To use the Tkinter we need to import the Tkinter module. As stated above, we have imported each file so that we can make function calls from our main file.
+
+from tkinter import *
+from PIL import ImageTk,Image #PIL -> Pillow
+import pymysql
+from tkinter import messagebox
+from AddBook import *
+from DeleteBook import *
+from ViewBooks import *
+from IssueBook import *
